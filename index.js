67e272e60e5d10858d108f23a4d7cea3a0552eaa
Alexander Cihla <AJC@AJs-Air.local>
2016-03-16 01:02:24 -0700

    justpack start

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,13 +11,28 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get('/db', function (request, response) {
+	    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		    client = new pg.Client;
+		    client.query('SELECT * FROM test_table', function(err, result) {
+			done();
+			if (err)
+			    { console.error(err); response.send("Error " + err); }
+			else
+			    { response.render('pages/db', {results: result.rows} ); }
+		    });
+	    });
+    })
+
 app.get('/', function(request, response) {
   response.render('pages/index');
+  require('bootstrap');
 });
 
 app.get('/cool', function(request, response) {
 	response.send(cool());
     });
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
