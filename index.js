1a488fd53eed08252f68877d3ef1041bcb78af96
Alexander Cihla <AJC@AJs-Air.local>
2016-03-17 23:17:05 -0700

    test deploy

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
-var app = express();
+var httpProxy = require('http-proxy');
 var pg = require('pg');
 
+var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -33,8 +34,34 @@ app.get('/cool', function(request, response) {
 	response.send(cool());
     });
 
+app.post('/request_trip', function(request, response) {
+	var travelers = request.travelers;
+	var departure = request.departure;
+	var price = request.price;
+	
+	var proxyOptions = {
+	    changeOrigin: true
+	};
+
+	httpProxy.prototype.onError = function (err) {
+	    console.log(err);
+	};
+
+	var apiProxy = httpProxy.createProxyServer(proxyOptions);
+	var apiForwardingUrl = 'https://docs.google.com/forms/d/1mbnp8aHHb19SQdb4kIl2cCLTHXrNIAzA_tyGELW961U/formResponse';
+	
+	console.log('got through vars with apiURL ' + apiForwardingUrl);
+	if ((travelers !== "") && (departure !== "") && ((price !== ""))) {
+		apiProxy.web(request, response, {target: apiForwardingUrl});
+	}
+	else {
+	    //Error message
+	}
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
 
